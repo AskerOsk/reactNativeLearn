c6a1342ef798d311552954bb3b020d32d7a39f0c
Oskar_Home <oskar1999asker@gmail.com>
2023-06-12 00:12:11 +0600

    Creat_Post_in_AppJS__and_creat_component_Post

--- App.js
+++ App.js
@@ -1,20 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar} from 'expo-status-bar';
+import { Text, View, Image } from 'react-native';
+import { Post } from './components/Post';
+import styled from 'styled-components/native'
+
+const MyStatusBar = styled.View`
+  margin-top: 50px;
+`;
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <MyStatusBar />
+      <StatusBar theme="auto"/>
+        <Post 
+          title='Test Работы' 
+          imageUrl="https://s1.1zoom.ru/b5050/243/Big_cats_Leopards_Snout_479812_1920x1200.jpg" 
+          createAt='11.06.2023 17:53'/>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
